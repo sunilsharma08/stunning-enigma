662c80033a297e85bb761158a46615bbed1a1d6a
Dharan Aditya <dharan.aditya@gmail.com>
2017-10-08 23:43:06 +0530

    Added GCD

---
+++ mathematical-algorithm/Gcd.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+func gcd(of a : Int,and b : Int) -> Int {
+    if b == 0{
+        return a
+    }else{
+        return gcd(of : b, and : a%b)
+    }
+}
+
+let a = 366
+let b = 60
+print("GDC of a = \(a) and b = \(b) is \(gcd(of : a,and : b))")
